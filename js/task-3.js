71b1de0fe224db764a392de5023e49fcf07260cb
sno0wi <sno0wi20@gmail.com>
2023-11-03 16:03:45 +0200

    done homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,45 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
+function checkForSpam(message) {
 
-  const totalWidth = contentWidth + 2 *(paddingWidth+borderWidth);
+  const lowerCaseMessage = message.toLowerCase();
 
-  return totalWidth;
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+/*function checkForSpam(message) {
+
+  const lowerCaseMessage = message.toLowerCase();
+
+  const resault = lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale") ? true : false;
+
+  return resault;
+}
+
+
+/*
+function checkForSpam(message) {
+
+  const lowerCaseMessage = message.toLowerCase();
+
+  switch (true) {
+    case lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale"):
+      return true;
+    default:
+      return false;
+  }
+
+}
+*/
+
+// Log to console
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
